ed380840496b0b12a853eac8b46dd764b909a2be
phucvin <phucvin2@gmail.com>
2018-09-18 00:29:28 +0700

    .

---
+++ Assets/Cloner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Cloner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject src = null;
+
+    private IEnumerator Start()
+    {
+        for (int i = 0, n = 30; i < n; ++i)
+        {
+            Instantiate(src, transform.position + (Vector3)(Vector2)Random.insideUnitSphere * 10, transform.rotation);
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
